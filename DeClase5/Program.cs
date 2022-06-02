5448285021ecf895bb282b0a394116a35f03630e
Sebastian <sebasfedele@gmail.com>
2022-06-01 23:20:24 -0300

    bucle while

---
+++ DeClase5/Program.cs
@@ -0,0 +1,15 @@
+﻿
+    string respuesta = "S";
+
+while(respuesta.ToUpper() != "N")
+    {
+    Console.Write("Ingrese su nombre: ");
+    string nombre = (Console.ReadLine());
+
+    Console.Write($"Hola {nombre} \n");
+
+
+    Console.WriteLine("Desea continuar? S/N");
+    respuesta = (Console.ReadLine());
+    
+}
